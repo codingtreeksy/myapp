df7cad61dca485567b1c879a73f6932847851c6e
codingtreeksy <codingtreeksy@gmail.com>
2022-08-09 22:01:18 +0900

    second

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -4,6 +4,7 @@ import { TestPros } from "../../types/Test";
 const Test= (props:TestPros)=>{
     return (
         <div>
+            <h1>수정</h1>
             {props.info.name} <br/>
             {props.info.age} <br/>
 
